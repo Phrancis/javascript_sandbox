40cfddaad649a88015a2edf39742bd58b69fe3da
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2016-03-22 07:17:46 -0400

    improve reducers.js with ...spread operators

--- app/components/ToDoList/reducers.js
+++ app/components/ToDoList/reducers.js
@@ -8,25 +8,23 @@ const initialState = {
 function todos(state = [], action) {
     switch(action.type) {
         case ADD_TODO:
-            return Object.assign({}, state, 
-            {
+            return { 
+                ...state,
                 todos: [
-                    ...state.todos, 
-                    {
+                    ...state.todos, {
                         text: action.text,
                         completed: false
                     }
                 ]
-            });
+            };
         case COMPLETE_TODO:
-            return Object.assign({}, state, 
-            {
+            return Object.assign({}, state, {
                 todos: state.todos.map((todo, index) => {
                     if (index === action.index) {
-                        return Object.assign({}, state, 
-                        {
+                        return { 
+                            ...state,
                             completed: true
-                        })
+                        };
                     }
                     return todo;   
                 })
@@ -36,19 +34,28 @@ function todos(state = [], action) {
     }
 }
 
+function visibilityFilter(state = SHOW_ALL, action) {
+    switch(action.type) {
+        case SET_VISIBILITY_FILTER:
+            return action.filter;
+        default: 
+            return state;
+    }
+}
+
 function toDoApp(state = initialState, action) {
     switch(action.type) {
         case SET_VISIBILITY_FILTER:
-            return Object.assign({}, state, 
-            {
+            return { 
+                ...state,
                 visibilityFilter: action.filter
-            });
+            };
         case ADD_TODO:
         case COMPLETE_TODO:
-            return Object.assign({}, state, 
-            {
+            return { 
+                ...state, 
                 todos: todos(state.todos, action)
-            });
+            };
         default:
             return state;
     }
